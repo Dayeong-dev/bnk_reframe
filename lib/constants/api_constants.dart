b36e1c36a1031962c79391f15d721afbb6a38ca2
Yun Dayeong <ydiosa98@gmail.com>
2025-08-05 15:39:51 +0900

    feat: Page 세팅

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String baseUrl = 'http://192.168.100.155';
+const String port = "8090";
+const String apiBaseUrl = '$baseUrl:$port'; // 최종 URL 조합
---
+++ lib/pages/auth/splash_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:local_auth/local_auth.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final _secureStorage = FlutterSecureStorage();
+  final _auth = LocalAuthentication();
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {  // 현재 프레임의 모든 위젯이 렌더링된 이후에 실행
+      _checkLoginStatus();
+    });
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final username = await _secureStorage.read(key: "username");
+    final biometricEnabled = await _secureStorage.read(key: "biometricEnabled");
+
+    if(username != null && biometricEnabled == 'true') {
+      final didAuthenticate = await _auth.authenticate(localizedReason: "생체 인증으로 로그인하세요.");
+
+      if(didAuthenticate) {
+        Navigator.pushReplacementNamed(context, "/home");
+        return;
+      }
+    }
+    // Navigator.pushReplacementNamed(context, "/login");
+    Navigator.pushReplacementNamed(context, "/home");
+
+    if(!mounted) {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}

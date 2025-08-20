f761da659dba277d7321f252445ea33977446eda
Yun Dayeong <ydiosa98@gmail.com>
2025-08-20 09:44:15 +0900

    feat: 충돌 수정

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,11 +1,8 @@
 // http://192.168.100.155
 // 192.168.100.124 수현학원 192.168.123.107 집
-//192.168.0.238 정원
+// 192.168.0.238 정원
 
-
-
-const apiUrl = "192.168.100.124";
-
+const apiUrl = "192.168.100.155";
 
 const apiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',

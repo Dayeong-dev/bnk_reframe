a2d0dd49c343eb299caa79611bc0d81db04e1815
Yun Dayeong <ydiosa98@gmail.com>
2025-10-24 23:12:26 +0900

    fix: apiUrl 데이터 제거

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,8 +1,4 @@
-// http://192.168.100.155
-// 192.168.100.18 수현학원 192.168.123.107 집
-// 192.168.0.238 정원
-
-const apiUrl = "192.168.0.12";
+const apiUrl = "[아이피 주소 입력란]";
 
 const apiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',
